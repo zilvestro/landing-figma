bfd6755252877c47a41beec6780976ad9b27b9c3
madilyani <madilyani1@gmail.com>
2025-06-18 18:53:27 +0500

    last changes fonts and stars

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Geist:wght@100..900&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         <div className={"rootVariables"}>
           <Header />
